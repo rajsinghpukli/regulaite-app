e8ea3f7aceff29973cb3d0d00d99d737caff5ef3
rajsinghpukli <rajsingh.pukli@gmail.com>
2025-09-09 00:01:52 +0800

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Hello", layout="wide")
+st.title("✅ Azure says hello")
+st.write("If you see this, infra is fine and the issue is inside your app code.")
